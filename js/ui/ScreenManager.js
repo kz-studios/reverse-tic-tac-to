32de51419973e4d9f19e627b6fa6aee19b0f28bd
KaizenCyrus <kz.studios.freelance@gmail.com>
2026-03-28 21:18:07 +0800

    Implement game setup screen and layout
    - Added the GameSetupScreen.js module.
    - Refactored the ScreenManager routing logic to transition to the game setup screen from the title menu instead of directly initializing the gameplay loop.
    - Renamed id '#screen-game-settings' to '#screen-game-setup'.

--- js/ui/ScreenManager.js
+++ js/ui/ScreenManager.js
@@ -1,13 +1,19 @@
 import TitleScreen from './screens/TitleScreen.js';
+import GameSetupScreen from './screens/GameSetupScreen.js';
 import GameplayScreen from './screens/GameplayScreen.js';
 
 export default class ScreenManager {
     constructor() {
         this.titleScreen = new TitleScreen();
+        this.gameSetupScreen = new GameSetupScreen();
         this.gameplayScreen = new GameplayScreen();
         this.screens = document.querySelectorAll('.screen');
 
         this.titleScreen.btnPlay.addEventListener('click', () => {
+            this.switch('screen-game-setup');
+        })
+
+        this.gameSetupScreen.btnStart.addEventListener('click', () => {
             this.gameplayScreen.clearGrids();
             this.gameplayScreen.renderGrids();
             this.switch('screen-gameplay');
@@ -16,19 +22,23 @@ export default class ScreenManager {
         // Dev functions
         document.addEventListener('keydown', (event) => {
             if (event.key === 'Escape') {
-                if (this.titleScreen.screen.classList.contains('active')) return;
-                this.switch('screen-title');
+                for (let i = 1; i < this.screens.length; i++) {
+                    if (this.screens[i].classList.contains('active')) {
+                        this.switch(this.screens[i-1].id);
+                        break;
+                    }
+                }
             }
         });
     }
 
     switch(targetScreen) {
-        this.screens.forEach((screen, _) => {
+        for (const screen of this.screens) {
             if (screen.id === targetScreen) {
                 screen.classList.replace('hidden', 'active');
             } else {
                 screen.classList.replace('active', 'hidden');
             }
-        });
+        }
     }
 }
---
+++ js/ui/screens/GameSetupScreen.js
@@ -0,0 +1,7 @@
+export default class GameSetupScreen {
+    constructor() {
+        this.screen = document.querySelector('#screen-game-setup');
+        this.btnBack = this.screen.querySelector('.btn-back');
+        this.btnStart = this.screen.querySelector('.btn-start');
+    }
+}
